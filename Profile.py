8163dfb253b8cec93109fd0e9689bf42b5a0db03
Cheyne LeVesseur, PhD <77408937+ProfessorLeVesseur@users.noreply.github.com>
2024-05-08 10:19:41 -0400

    Update Profile.py

--- Profile.py
+++ Profile.py
@@ -5,6 +5,45 @@ import streamlit as st
 from PIL import Image
 import random
 
+
+-------
+import logging
+import shutil
+import pathlib
+from bs4 import BeautifulSoup
+
+def add_analytics_tag():
+    # Replace G-XXXXXXXXXX with your actual Google Analytics tracking ID
+    analytics_js = """
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZE6TBWRFKS"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-ZE6TBWRFKS');
+    </script>
+    """
+    analytics_id = "G-ZE6TBWRFKS"
+
+    # Identify HTML path of Streamlit
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=analytics_id): # if id not found within HTML file
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # backup recovery
+        else:
+            shutil.copy(index_path, bck_index)  # save backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + analytics_js) 
+        index_path.write_text(new_html) # insert analytics tag at top of head
+
+# Call the function to add the analytics tag
+add_analytics_tag()
+-------
+
 # Streamlit page setup
 icon = Image.open("MTSS.ai_Icon.png")
 st.set_page_config(
