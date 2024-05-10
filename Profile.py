55e2a2a1e323c35b8120b3121f19d2cf63cff651
Cheyne LeVesseur, PhD <77408937+ProfessorLeVesseur@users.noreply.github.com>
2024-05-10 12:00:01 -0400

    Update Profile.py

--- Profile.py
+++ Profile.py
@@ -3,10 +3,15 @@
 #------------------------------------------------------------------------
 import streamlit as st
 from PIL import Image
-import random
+from endorsement_data import endorsements
+import streamlit.components.v1 as components
+# import random
 
 
-#-------
+#------------------------------------------------------------------------
+# Google Analytics
+#------------------------------------------------------------------------
+
 import logging
 import shutil
 import pathlib
@@ -42,7 +47,11 @@ def add_analytics_tag():
 
 # Call the function to add the analytics tag
 add_analytics_tag()
-#-------
+
+
+#------------------------------------------------------------------------
+# Page Setup
+#------------------------------------------------------------------------
 
 # Streamlit page setup
 icon = Image.open("MTSS.ai_Icon.png")
@@ -59,6 +68,7 @@ st.set_page_config(
 # with open("style.css") as f:
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
 
 #------------------------------------------------------------------------
 # Header
@@ -80,6 +90,7 @@ st.write('''
 col1, col2, col3 = st.columns([1,1,1])
 with col2:
     st.image("ProfilePic.png", use_column_width=True)
+
 
 #------------------------------------------------------------------------
 # LLM
@@ -104,6 +115,7 @@ with col2:
     
 #     st.divider()
 
+
 #------------------------------------------------------------------------
 # Introduction
 #------------------------------------------------------------------------
@@ -118,6 +130,141 @@ Published author and presenter, contributing substantial research to the field o
 
 # st.markdown(
 #   "##### Connect with LinkedIn[![LinkedIn](https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg)](https://www.linkedin.com/in/levesseur/)")
+
+
+#------------------------------------------------------------------------
+# Endorsements
+#------------------------------------------------------------------------
+
+with st.container():
+    # Divide the container into three columns
+    # col1,col2,col3 = st.columns([0.475, 0.475, 0.05])
+    col1,col2,col3 = st.columns([0.675, 0.275, 0.05])
+    # In the first column (col1)        
+    with col1:
+        # Add a subheader to introduce the coworker endorsement slideshow
+        # st.subheader("Coworker Endorsements")
+        # Embed an HTML component to display the slideshow
+        components.html(
+        f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Styles for the slideshow -->
+        <style>
+            * {{box-sizing: border-box;}}
+            .mySlides {{display: none;}}
+            img {{vertical-align: middle;}}
+
+            /* Slideshow container */
+            .slideshow-container {{
+            position: relative;
+            margin: auto;
+            width: 100%;
+            }}
+
+            /* The dots/bullets/indicators */
+            .dot {{
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #eaeaea;
+            border-radius: 50%;
+            display: inline-block;
+            transition: background-color 0.6s ease;
+            }}
+
+            .active {{
+            background-color: #6F6F6F;
+            }}
+
+            /* Fading animation */
+            .fade {{
+            animation-name: fade;
+            animation-duration: 1s;
+            }}
+
+            @keyframes fade {{
+            from {{opacity: .4}} 
+            to {{opacity: 1}}
+            }}
+
+            /* On smaller screens, decrease text size */
+            @media only screen and (max-width: 300px) {{
+            .text {{font-size: 11px}}
+            }}
+            </style>
+        </head>
+        <body>
+            <!-- Slideshow container -->
+            <div class="slideshow-container">
+                <div class="mySlides fade">
+                <img src={endorsements["img1"]} style="width:100%">
+                </div>
+
+                <div class="mySlides fade">
+                <img src={endorsements["img2"]} style="width:100%">
+                </div>
+
+                <div class="mySlides fade">
+                <img src={endorsements["img3"]} style="width:100%">
+                </div>
+
+            </div>
+            <br>
+            <!-- Navigation dots -->
+            <div style="text-align:center">
+                <span class="dot"></span> 
+                <span class="dot"></span> 
+                <span class="dot"></span> 
+            </div>
+
+            <script>
+            let slideIndex = 0;
+            showSlides();
+
+            function showSlides() {{
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {{
+                slides[i].style.display = "none";  
+            }}
+            slideIndex++;
+            if (slideIndex > slides.length) {{slideIndex = 1}}    
+            for (i = 0; i < dots.length; i++) {{
+                dots[i].className = dots[i].className.replace("active", "");
+            }}
+            slides[slideIndex-1].style.display = "block";  
+            dots[slideIndex-1].className += " active";
+            }}
+
+            var interval = setInterval(showSlides, 4000); // Change image every 4 seconds
+
+            function pauseSlides(event)
+            {{
+                clearInterval(interval); // Clear the interval we set earlier
+            }}
+            function resumeSlides(event)
+            {{
+                interval = setInterval(showSlides, 4000);
+            }}
+            // Set up event listeners for the mySlides
+            var mySlides = document.getElementsByClassName("mySlides");
+            for (i = 0; i < mySlides.length; i++) {{
+            mySlides[i].onmouseover = pauseSlides;
+            mySlides[i].onmouseout = resumeSlides;
+            }}
+            </script>
+
+            </body>
+            </html> 
+
+            """,
+                height=270,
+    )  
+
 
 #------------------------------------------------------------------------
 # Navigation bar
@@ -159,6 +306,7 @@ st.markdown("""
 </nav>
 """, unsafe_allow_html=True)
 
+
 #------------------------------------------------------------------------
 # Function for columns/text layout
 #------------------------------------------------------------------------
@@ -199,7 +347,8 @@ def txt4(a, b, c):
     st.markdown(b)
   with col3:
     st.markdown(c)
-     
+
+
 #------------------------------------------------------------------------
 # Projects
 #------------------------------------------------------------------------
@@ -229,7 +378,8 @@ st.success('- [Stxtement | Alt Text + Images](http://www.stxtement.com)')
 st.success('- [MTSS Map Maker | ISD District PSA](https://mtssmaps-4320d9f0e6fd.herokuapp.com)')
 st.success('- [Prxmpting | Prompt Engineering](https://huggingface.co/spaces/ProfessorLeVesseur/Prxmpting)')
 st.success('- [MTSS GPT | Conversational AI Language Model](https://mtssgpt-6204cd3bf5a6.herokuapp.com/)')
-    
+
+
 #------------------------------------------------------------------------
 # Education
 #------------------------------------------------------------------------
@@ -276,6 +426,7 @@ st.markdown('''
 - Research Assistant: `Psychopharmacology Lab` (PI: Keith Williams, Ph.D.)
 - Research Assistant: `Emotion and Perception Lab` (PI: Dean Purcell, Ph.D.)
 ''')    
+
 
 #------------------------------------------------------------------------
 # Career
@@ -367,9 +518,11 @@ I focused on `conducting psychological assessments and evaluations`. I developed
 - Led MTSS implementation and management.
 ''')
 
+
 #------------------------------------------------------------------------
 # Scholarship
 #------------------------------------------------------------------------
+
 ##################### White space for navigation bar
 st.markdown('''
 <style>
@@ -403,9 +556,11 @@ txt4('2015', 'Russell, C., Ward, c., Harms, A., St. Martin, K., Cusumano, D., Fi
 txt4('2015', '**LeVesseur, C.**, (2015) *Implementing Universal Social and Emotional Learning Programs: The Development, Validation, and Inferential Findings from the Schoolwide SEL Capacity Assessment.* Doctoral Dissertations', 'https://scholarworks.umass.edu/cgi/viewcontent.cgi?article=1459&context=dissertations_2')
 txt4('2013', 'Whitcomb, S.A., & **LeVesseur, C.** (2012). *Promoting student strengths and assets.* In. S.E. Brock, P.J. Lazarus, & S.R. Jimerson (Eds.), Best practices in crisis prevention and intervention (2nd edition), (pp. 97-114). Bethesda, MD: National Association of School Psychologists.', 'https://www.nasponline.org/books-and-products/products/books/titles/best-practices-in-school-crisis-prevention-and-intervention')
 
+
 #------------------------------------------------------------------------
 # Skills
 #------------------------------------------------------------------------
+
 ##################### White space for navigation bar
 st.markdown('''
 <style>
@@ -435,9 +590,11 @@ txt3('Machine Learning', '`scikit-learn`')
 txt3('Web development', '`HTML`, `CSS`')
 txt3('Model deployment', '`streamlit`, `Heroku`, `Hugging Face`')
 
+
 #------------------------------------------------------------------------
 # Connect
 #------------------------------------------------------------------------
+
 ##################### White space for navigation bar
 st.markdown('''
 <style>
