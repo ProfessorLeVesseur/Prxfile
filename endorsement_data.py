7daf8b9d6a877d7a26037e3bb0ccfbbbc17c1231
Cheyne LeVesseur, PhD <77408937+ProfessorLeVesseur@users.noreply.github.com>
2024-05-10 11:56:41 -0400

    Add files via upload

---
+++ endorsement_data.py
@@ -0,0 +1,8 @@
+endorsements = {
+    "img1": "https://github.com/ProfessorLeVesseur/Prxfile/blob/main/endorsements/endorsements_1.jpg?raw=true",
+    "img2": "https://github.com/ProfessorLeVesseur/Prxfile/blob/main/endorsements/endorsements_2.jpg?raw=true",
+    "img3": "https://github.com/ProfessorLeVesseur/Prxfile/blob/main/endorsements/endorsements_3.jpg?raw=true"
+}
+
+
+
